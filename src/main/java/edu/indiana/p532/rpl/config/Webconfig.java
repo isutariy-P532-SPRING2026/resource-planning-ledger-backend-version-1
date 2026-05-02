6bac2827d3d63911e12f44ea0ab8b44e0a6e9d8b
Ishita Sutariya <worksutariya@gmail.com>
2026-05-02 16:59:27 -0400

    Backend connection error solved

--- src/main/java/edu/indiana/p532/rpl/config/Webconfig.java
+++ src/main/java/edu/indiana/p532/rpl/config/Webconfig.java
@@ -14,7 +14,7 @@ public class Webconfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://resource-planning-ledger-frontend.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
